6fbde02841d03fa76f620bf0cad132b4bf20d8b1
basit <basitng2004@gmail.com>
2022-07-12 02:06:30 -0700

    added loaded

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
+import React from "react";
+import animationData from "../public/lf30_editor_fz5fsybt.json";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Lottie from "react-lottie";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Box } from "@mui/material";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isLoaded, setLoaded] = React.useState(false);
   const theme = createTheme({
     typography: {
       fontFamily: "ngo",
@@ -20,8 +25,42 @@ function MyApp({ Component, pageProps }: AppProps) {
       },
     },
   });
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  React.useEffect(() => {
+    window.onload = (e) => {
+      setLoaded(true);
+    };
+  });
+
   return (
     <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          width: "100%",
+          height: "100%",
+          position: "fixed",
+          zIndex: 100,
+          background: "#fff",
+          display: isLoaded ? "grid" : "none",
+          placeContent: "center",
+        }}
+      >
+        <Lottie
+          options={defaultOptions}
+          height={400}
+          width={400}
+          isStopped={false}
+          isPaused={false}
+        />
+      </Box>
       <Component {...pageProps} />
     </ThemeProvider>
   );
